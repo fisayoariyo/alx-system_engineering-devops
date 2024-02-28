76b820c2317043fcb9e8f89a93383d2c9e129837
Adefisayomi <ariyoadefisayomi@gmail.com>
2024-02-28 22:23:02 +0000

    0x16

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""
-no of subs
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    if not isinstance(subreddit, str) or not subreddit:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    
-    user_agent = "Custom User Agent"
-    headers = {"User-Agent": user_agent}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        """
-        to raise an error for bad response status
-        """
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return 0
-
-
+    results = response.json().get("data")
+    return results.get("subscribers")
