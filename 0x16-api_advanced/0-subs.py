11b5dac368acc6c3ed68ec64d49fac0af7e4320f
Adefisayomi <ariyoadefisayomi@gmail.com>
2024-03-01 16:01:54 -0800

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""" Prints the subscribers to subreddit """
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers """
-    headers = {'User-Agent': 'PPP/0.0.1'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url, headers=headers)
-    if req.status_code != 200:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    js = req.json()
 
-    return js.get('data').get('subscribers')
+# Testing the function
+if __name__ == '__main__':
+    subreddit = input("Enter the subreddit name: ")
+    print(number_of_subscribers(subreddit))
+
