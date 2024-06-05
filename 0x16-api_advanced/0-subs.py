138ec2c4680f8a9ad7bd39356eef6784bbd883c9
Adefisayomi <ariyoadefisayomi@gmail.com>
2024-06-04 17:22:45 -0700

    0x16 api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""Module for querying the Reddit API to get the number of subscribers of a subreddit"""
 
+import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """Queries the Reddit API and returns the number of subscribers to the subreddit
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers to the subreddit, or 0 if
+        the request fails or the subreddit does not exist.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    
+    try:
+        response = requests.get(url, headers={"User-Agent": "reddit-subscriber-counter"})
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
 
