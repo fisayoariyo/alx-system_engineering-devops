2e532a6145cb297bcd59ca921656cf2de92a4423
Adefisayomi <ariyoadefisayomi@gmail.com>
2024-08-29 19:35:47 -0700

    fixed wordpress server bug in php file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # fixes a bug in wp-setings.php
-
+# by replacing mistyped word in /var/www/html/wp-settings.php
+#
 exec { 'fix the php extension issue':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/',
 }
